623c4d54e76de7660309caad4df178bed1f358ff
vani <vanishreeloni123@gmail.com>
2026-01-19 01:53:13 -0800

    added ID

--- provider.tf
+++ provider.tf
@@ -8,15 +8,5 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    CLIENT_ID       = "1770a913-438f-4293-8255-03560641900b"
-    CLIENT_SECRET   = "nLs8Q~noLUIlhBdecxOUkhZBJjwITg8eF~pLjblq"
-    TENANT_ID       = "60fb6db9-dc95-4fc8-b91b-2ca9c1b8971f"
-    SUBSCRIPTION_ID = "c46beff3-d22c-4a6f-82a7-693fd94c69c4"
-
-
-
-
-
-  }
+  features {} 
 }
