2e925db35a7ab2c64c4eac8424840ab89a71431e
vani <vanishreeloni123@gmail.com>
2026-01-19 02:16:43 -0800

    main.tf file

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "tfazgit-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
+  size                = "Standard_DS1_v2" 
   admin_username      = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.nic.id
